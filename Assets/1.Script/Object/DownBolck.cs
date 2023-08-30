68187fc01afdcfacbc9afe3b72e9cbadef6d5f2a
Woo jea woo <139198446+RhyJeaWoo@users.noreply.github.com>
2023-08-30 13:45:33 +0900

    ddd

--- Assets/1.Script/Object/DownBolck.cs
+++ Assets/1.Script/Object/DownBolck.cs
@@ -1,33 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DownBolck : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] float time;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-      
-        rb.bodyType = RigidbodyType2D.Kinematic;
-        
+
+        rb.bodyType = RigidbodyType2D.Static;
+        rb.gravityScale = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(time > 0.5f)
-            rb.bodyType = RigidbodyType2D.Dynamic;
-    }
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player"))
+
+        if (rb.bodyType == RigidbodyType2D.Dynamic)
         {
             time += Time.deltaTime;
+        }
+        else if (time > 1f)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * time);
           
         }
+
+
+
+
     }
+
+
+
+
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        time = 0;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
+
+
+
+
+
 }

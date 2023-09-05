08118b172da784bb44cf2343f330669ca9dad577
yunjuju <113872746+yunjuju@users.noreply.github.com>
2023-09-05 10:34:58 +0900

    플레이어 애니메이션 고침

--- Assets/1.Script/Map/Map_YJ.cs
+++ Assets/1.Script/Map/Map_YJ.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class Map_YJ : Map
 {
-    Vector2[] playerPosition = new Vector2[4];
-    public virtual void SetMapInfo(string SceneName, string MapName, string MapSubName,
-        Vector2 player1_pos, Vector2 player2_pos, Vector2 player3_pos, Vector2 player4_pos)
+    [SerializeField] Vector2[] playerPosition = new Vector2[4];
+    [SerializeField] GameObject[] playerPrefabs;
+
+    public override void SetMapInfo(string SceneName, string MapName, string MapSubName, Vector2 player1_pos, Vector2 player2_pos, Vector2 player3_pos, Vector2 player4_pos)
     {
+        base.SetMapInfo(SceneName, MapName, MapSubName, player1_pos, player2_pos, player3_pos, player4_pos);
+
         this.Scene_name = SceneName;
         this.Map_name = MapName;
         this.Map_subTitle = MapSubName;
@@ -17,30 +20,36 @@ public class Map_YJ : Map
         playerPosition[1] = player2_pos;
         playerPosition[2] = player3_pos;
         playerPosition[3] = player4_pos;
-
-
     }
 
     public override void Start()
     {
-       // SpawnPlayer();
+        base.Start();
     }
 
-    //public override void SpawnPlayer()
-    //{
-    //    var pl = PhotonNetwork.PlayerList;
+    public void SpawnPlayer()
+    {
+        var pl = PhotonNetwork.PlayerList;
 
-    //    Debug.Log("스폰플레이어 실행됨");
-    //    for (int i = 0; i < pl.Length; ++i)
-    //    {
-    //        if (pl[i].IsLocal)
-    //        {
-    //            Debug.Log("플레이어 스폰 시도" + pl[i].UserId + " " + i);
-    //            PhotonNetwork.Instantiate("planeBlue", playerPosition[i], Quaternion.identity);
+        Debug.Log("스폰플레이어 실행됨");
+        for (int i = 0; i < pl.Length; ++i)
+        {
+            if (pl[i].IsLocal)
+            {
+                Debug.Log("플레이어 스폰 시도" + pl[i].UserId + " " + i);
 
-    //        }
-    //    }
+                GameObject playerObject = PhotonNetwork.Instantiate(playerPrefabs[i].name, playerPosition[i], Quaternion.identity);
+                PhotonView photonView = playerObject.GetComponent<PhotonView>();
 
-    //}
+                PhotonNetwork.Instantiate("planeBlue", playerPosition[i], Quaternion.identity);
+                PhotonNetwork.Instantiate("planeRed", playerPosition[i], Quaternion.identity);
+                PhotonNetwork.Instantiate("planeGreen", playerPosition[i], Quaternion.identity);
+                PhotonNetwork.Instantiate("planePurple", playerPosition[i], Quaternion.identity);
 
+                photonView.TransferOwnership(pl[i]);
+
+            }
+        }
+
+    }
 }

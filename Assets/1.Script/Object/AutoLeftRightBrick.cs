09ff6ee22dc527275d5ba2b0033c0a76a21d5dae
nikemanian <142286333+nikemanian@users.noreply.github.com>
2023-09-06 16:44:00 +0900

    ㅇ
    ㅇㅇ

--- Assets/1.Script/Object/AutoLeftRightBrick.cs
+++ Assets/1.Script/Object/AutoLeftRightBrick.cs
@@ -4,7 +4,7 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class AutoLeftRightBrick : MonoBehaviour
 {
     Vector3 pos; //현재위치
-   public float delta = 5.0f; // 좌(우)로 이동가능한 (x)최대값
+   public float inputX = 5.0f; // 좌(우)로 이동가능한 (x)최대값
     public float speed = 0.9f; // 이동속도
 
     // Start is called before the first frame update
@@ -17,7 +17,7 @@ public class AutoLeftRightBrick : MonoBehaviour
     void Update()
     {
         Vector3 v = pos;
-        v.x += delta * Mathf.Sin(Time.time * speed);
+       // v.x += in * Mathf.Sin(Time.time * speed);
         transform.position = v;
     }
 }

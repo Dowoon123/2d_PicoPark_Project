d9f284ca519d52e8f5d8d5115fa618d940703bcb
Woo jea woo <139198446+RhyJeaWoo@users.noreply.github.com>
2023-09-08 10:07:57 +0900

    수정

--- Assets/1.Script/Object/Stage_Cannon/Cannon.cs
+++ Assets/1.Script/Object/Stage_Cannon/Cannon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Cannon : MonoBehaviour
 {
@@ -12,38 +13,53 @@ public class Cannon : MonoBehaviour
 
     [SerializeField] private bool isShot;
 
+    // Start에서 코루틴을 변수?/??????
+
+    Coroutine shotCoroutine;
+
+    bool isEndShoot = false;
+
     void Start()
     {
 
         KeyBottle = GameObject.Find("KeyBottle");
         KeyBottle.GetComponent<KeyBottle>();
         isShot = false;
+
+        GetComponent<PhotonView>().RPC("Shot", RpcTarget.All);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isShot && KeyBottle.GetComponent<KeyBottle>().Hp > 0)
+        if (!isShot && KeyBottle.GetComponent<KeyBottle>().Hp <= 0 && !isEndShoot)
         {
-            Shot();
+            GetComponent<PhotonView>().RPC("StopShot", RpcTarget.All);
+            isEndShoot = true;
         }
         else if(KeyBottle.GetComponent<KeyBottle>() == null)
             return;
       
     }
 
+    [PunRPC]
     private void Shot()
     {
-       
-            Instantiate(bullet, shotPos.transform.position, Quaternion.identity);
-            StartCoroutine(isShoted());
-        
+
+        shotCoroutine = StartCoroutine(ShotBullet());
 
     }
 
-    IEnumerator isShoted()
+    [PunRPC]
+    private void StopShot()
+    {
+        StopCoroutine(shotCoroutine);
+    }
+
+    IEnumerator ShotBullet()
     {
         isShot = true;
+        Instantiate(bullet, shotPos.transform.position, Quaternion.identity);
         yield return new WaitForSeconds(3f);
         isShot = false;
     }

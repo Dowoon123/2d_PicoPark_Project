e147dfb202f0d82837eaa341748aa3126f2777c6
Dowoon123 <wmfahgo@naver.com>
2023-09-08 11:46:30 +0900

    ㅇㅂㅇ
    ㅂㅈㅇㅂㅇㅁㅇ

--- Assets/1.Script/Object/AutoLeftRightBrick.cs
+++ Assets/1.Script/Object/AutoLeftRightBrick.cs
@@ -4,22 +4,43 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class AutoLeftRightBrick : MonoBehaviourPunCallbacks
 {
+    Vector3 goalPos;
+    Vector3 originPos;
     Vector3 pos; //현재위치
-   public float inputX = 5.0f; // 좌(우)로 이동가능한 (x)최대값
+    public float inputX = 5.0f; // 좌(우)로 이동가능한 (x)최대값
     public float speed = 0.9f; // 이동속도
+
+    bool isRight = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        pos = transform.position;
+        originPos = transform.position;
+        goalPos = originPos;
+        goalPos.x += inputX;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 v = pos;
-        v.x += inputX * Mathf.Sin(Time.time * speed);
-        transform.position = v;
+        if (isRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            var dist = Vector2.Distance(goalPos, transform.position);
+            if (dist <= 0.5f)
+                isRight = false;
+
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            var dist = Vector2.Distance(originPos, transform.position);
+            if (dist <= 0.5f)
+                isRight = true;
+        }
     }
 }
 

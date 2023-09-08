6b6cffb5eea73310af6411010a671257ba7d38ea
Dowoon123 <wmfahgo@naver.com>
2023-09-08 10:14:44 +0900

    ㅁㄴㅇ
    ㅁㄴㄻㄴㄹ

--- Assets/1.Script/Map/Map.cs
+++ Assets/1.Script/Map/Map.cs
@@ -167,7 +167,7 @@ public class Map : MonoBehaviourPunCallbacks
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PhotonNetwork.LoadLevel(StageSelectScene);
+            PhotonNetwork.LoadLevel("StageSelectScene");
 
         }
     }
